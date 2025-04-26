78c6071248818a0e46bb9985052a5e576bd83e5c
Duel <emanuelthadeu45@gmail.com>
2025-04-26 11:25:31 -0300

    firebaseConfig added

---
+++ api/firebase-env.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA1WQcCvzlXGbZPdymqumZ8UONWSIHTa0I",
+    authDomain: "software-engineering-6109e.firebaseapp.com",
+    projectId: "software-engineering-6109e",
+    storageBucket: "software-engineering-6109e.firebasestorage.app",
+    messagingSenderId: "201759986310",
+    appId: "1:201759986310:web:2bb57548af24881dd9b707",
+    measurementId: "G-FD3MHPSRDV"
+  };
